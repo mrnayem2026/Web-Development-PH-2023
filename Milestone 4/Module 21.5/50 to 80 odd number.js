11c0d88bd999f64c37f21aae9beb54e76e4c4762
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-02-01 19:13:51 +0600

    3 js simple problem and solution added

---
+++ Milestone 4/Module 21.5/50 to 80 odd number.js
@@ -0,0 +1,4 @@
+for(let i=51; i<=79; i+=2)
+{
+    console.log(i);
+}
---
+++ Milestone 4/Module 21.5/sum 3 number.js
@@ -0,0 +1,7 @@
+function sum3Number(num1,num2,num3)
+{
+    var sum = num1+num2+num3;
+    return sum;
+}
+var result = sum3Number(1,1,1);
+console.log(result);
