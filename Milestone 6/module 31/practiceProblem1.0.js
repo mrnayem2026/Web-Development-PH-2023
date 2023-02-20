c193a96baaa5e9897fc7de0036c50936e0e062ba
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-02-20 07:40:23 +0600

    practice proble added

---
+++ Milestone 6/module 31/practiceProblem1.0.js
@@ -0,0 +1,4 @@
+const multiply = (num1,num2,num3) =>  num1*num2*num3;
+
+let multiplyThreeNumber = multiply(100,200,300);
+console.log(multiplyThreeNumber);
---
+++ Milestone 6/module 31/practiceProblem1.1.js
@@ -0,0 +1,8 @@
+const updateName = (passion,habbit,eat) => {
+    return `I am a ${passion}. I love to ${habbit} and I love to ${eat}`
+}
+
+let nayem = updateName("Web DeveLoper", "Code", "Briyani");
+console.log(nayem);
+let nayem1 = updateName("content creator", "Love", "Ice cream");
+console.log(nayem1);
