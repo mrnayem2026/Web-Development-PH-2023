1cc96c6f9f478f4b04983fb37ef75b37400654a9
nahid <isratjaman913@gmail.com>
2023-03-21 11:42:34 +0600

    React explore agine

---
+++ Millestone 8/my-react-app/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import {HelloNayem,BookName} from './commponent/helloNamey'
+import { IncrementNumber } from './commponent/State'
+import { User } from './commponent/JosnExplor'
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <HelloNayem name="Mostafizur Rahman Nayem" village="Dotterabad" postOffice="Dotterabad" polliceOffice="Agulzhara" distric="Barishal"></HelloNayem>
+      <HelloNayem name="Mohammad Najim" village="Dotterabad" postOffice="Dotterabad" polliceOffice="Agulzhara" distric="Barishal"></HelloNayem>
+      <BookName ></BookName> */}
+
+      {/* <h1>Increment  Number is blow</h1>
+      <IncrementNumber></IncrementNumber> */}
+
+      <User></User>
+    </div>
+  )
+}
+
+
+
+
+export default App
