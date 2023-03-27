e735d241a726e293502da9fa04326420997955c0
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-03-27 10:14:39 +0600

    Ema jhon simpale site header section added

---
+++ Millestone 8/ema-jhon/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../assets/images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className=' bg-slate-700 text-white'>
+            <div className=' container mx-auto flex justify-between py-4'>
+                <img src={logo} alt="" />
+                <div>
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Order</a></li>
+                        <li><a>Order Review</a></li>
+                        <li><a>Manage Inventory</a></li>
+                        <li><a>Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
