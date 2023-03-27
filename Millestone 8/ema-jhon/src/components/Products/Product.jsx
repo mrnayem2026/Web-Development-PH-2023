4c688e828224bea9042535cc83d0d4e7b0a5d65e
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-03-27 11:13:04 +0600

    Product added from fake json file

---
+++ Millestone 8/ema-jhon/src/components/Products/Product.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Product.css'
+const Product = (props) => {
+    console.log(props.product);
+    const { img, name, price, ratings, seller } = props.product;
+    return (
+        <>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title text-2xl font-semibold">{name}</h2>
+                    <p className='text-lg font-medium'>Price: ${price}</p>
+                    <div>
+                        <p>Manufacturer :{seller}</p>
+                        <p>Rating : {ratings} start</p>
+                    </div>
+                    <div className="card-actions ">
+                        <a href="#"><button className="btn btn-primary">Add to Cart <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                        </svg>
+                        </button></a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Product;
--- Millestone 8/ema-jhon/src/components/Shop/Shop.jsx
+++ Millestone 8/ema-jhon/src/components/Shop/Shop.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Products/Product';
 import './Shop.css'
 const Shop = () => {
+    const [products,setProducts] = useState([]);
+
+    useEffect(()=>{
+        const fetchData =  async()=>{
+            const res = await fetch('../../../public/fakeData/products.json');
+            const data = await res.json();
+            setProducts(data);
+        }
+        fetchData();
+    },[])
     return (
         <>
             <div className='grid  lg:grid-cols-12'>
-                <div className='col-span-10  ml-48'>
-                    
+                <div id='product-container' className='col-span-10  ml-48 grid grid-cols-3 gap-6 pt-40'>
+                    {
+                        products.map((product)=><Product key={product.id}  product={product}></Product>)
+                    }
                 </div>
                 <div className='col-span-2  pt-6'>
                     <h1 className='text-2xl font-bold'>Order Summary</h1>
