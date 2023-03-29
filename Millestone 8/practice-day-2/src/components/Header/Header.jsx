0f6cf2c20ca03e15303fbffb8b72619b9cf0d0a5
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-03-29 16:42:13 +0600

    The meal day header section added

--- Millestone 8/practice-day-2/src/components/Header/Header.jsx
+++ Millestone 8/practice-day-2/src/components/Header/Header.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            Hello  I am from Header
-        </div>
+        <>
+            <div className='container mx-auto'>
+                <div className="navbar my-10">
+                    <div className="flex-1">
+                        <a className="btn btn-ghost normal-case text-xl"><img src="https://www.themealdb.com/images/logo-small.png" alt="TheMealDB"/></a>
+                    </div>
+                    <div className="flex-none gap-2">
+                        <div className="form-control">
+                            <input type="text" placeholder="Search by meal name" className="input input-bordered" />
+                        </div>
+                        <div className="dropdown dropdown-end">
+                            <label className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src="https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg" />
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
