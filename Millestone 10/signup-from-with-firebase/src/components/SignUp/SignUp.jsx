cdea9f153e5c94d588adb4708cda37659ef20fd1
Mostafizur Rahman nayem <hpbd17062022@gmail.com>
2023-04-16 09:29:38 +0600

    All UI Desing done

--- Millestone 10/signup-from-with-firebase/src/components/SignUp/SignUp.jsx
+++ Millestone 10/signup-from-with-firebase/src/components/SignUp/SignUp.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import singUpPic from '../../assets/icon/signupPic.png'
+import facebook from '../../assets/icon/facebook.png'
+import github from '../../assets/icon/github.png'
+import google from '../../assets/icon/google.png'
+import twitter from '../../assets/icon/twitter.png'
 
 const SignUp = () => {
     return (
@@ -7,13 +11,20 @@ const SignUp = () => {
             <div className="card  lg:card-side bg-base-100 shadow-xl">
                 <figure><img src={singUpPic} className='w-fit  bg-gradient-to-r from-cyan-500 to-blue-500' alt="Album" /></figure>
                 <div className="card-body">
-                    <h2 className="text-center text-5xl font-mono font-bold py-7 text-blue-600">Login your account</h2>
+                    <h2 className="text-center text-5xl font-mono font-bold py-7 text-blue-600">Singup your account</h2>
                     <div className='flex flex-col items-center'>
                         <input type="text" placeholder='Username' className='text-2xl p-3 focus:outline-none placeholder-blue-600 bg-base-100 border-b-4 border-blue-600 w-2/4' />
                         <input type="password" placeholder='Password' className='text-2xl p-3 focus:outline-none placeholder-blue-600 bg-base-100 border-b-4 border-blue-600 w-2/4' />
                     </div>
-                    <div className="card-actions ">
-                        <button className="btn btn-primary">Login</button>
+                    <div className="card-actions justify-center py-4">
+                        <button className="btn btn-primary bg-gradient-to-r from-cyan-500 to-blue-500 text-white w-2/6 tracking-widest font-black text-xl">Singup</button>
+                    </div>
+
+                    <div className='flex flex-col justify-center items-center gap-1'>
+                        <button className='btn btn-primary rounded-lg  bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 border-0 text-black w-2/6'><img src={facebook} alt="facebook icon" className='w-10 pr-2 ' />Singup with facebook</button>
+                        <button className='btn btn-primary rounded-lg  bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 border-0 text-black w-2/6'><img src={github} alt="github icon"  className='w-10 pr-2'/>Singup with github</button>
+                        <button className='btn btn-primary rounded-lg  bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 border-0 text-black w-2/6'><img src={google} alt="google icon"  className='w-10 pr-2'/>Singup with google</button>
+                        <button className='btn btn-primary rounded-lg  bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 border-0 text-black w-2/6'><img src={twitter} alt="twitter icon "  className='w-10 pr-2' />Singup with twitter</button>
                     </div>
                     <div>
 
