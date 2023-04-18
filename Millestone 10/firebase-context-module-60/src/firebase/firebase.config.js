80d2c734e8f9fb16908a480b0a65e360f3403c5f
Mostafizur Rahman nayem <hpbd17062022@gmail.com>
2023-04-18 14:05:01 +0600

    Firebase,contextApi and Private route empliment

---
+++ Millestone 10/firebase-context-module-60/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxogF547PhFc2jldrvi6OVT81EIL4IYVc",
+  authDomain: "fir-context-module-60.firebaseapp.com",
+  projectId: "fir-context-module-60",
+  storageBucket: "fir-context-module-60.appspot.com",
+  messagingSenderId: "581637340142",
+  appId: "1:581637340142:web:7a54b97875a6548bc3acff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ Millestone 10/firebase-context-module-60/src/providers/AuthProvides.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createContext } from 'react';
+
+const AuthContext = createContext()
+
+
+const AuthProvides = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthProvides;
