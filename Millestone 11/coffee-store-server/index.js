5e3fba03a3f138e49d0ba5e489cc784ce8cc6242
Mostafizur Rahman nayem <hpbd17062022@gmail.com>
2023-05-17 11:21:56 +0600

    Update a coffee successful

--- Millestone 11/coffee-store-server/index.js
+++ Millestone 11/coffee-store-server/index.js
@@ -39,27 +39,49 @@ async function run() {
     })
 
     // get all  data from database
-    app.get('/coffees', async(req,res)=>{
-       const corsor = coffeeCollection.find();
-       const result = await corsor.toArray();
-       res.send(result)
+    app.get('/coffees', async (req, res) => {
+      const corsor = coffeeCollection.find();
+      const result = await corsor.toArray();
+      res.send(result)
     })
 
     // get specific data from database by id 
-    app.get('/coffees/:id', async(req,res)=>{
+    app.get('/coffees/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new Object(id)}
+      const query = { _id: new Object(id) }
       const result = coffeeCollection.findOne(query);
       res.send(result);
     })
 
     // delete data from database 
-    app.delete('/coffees/:id', async(req,res)=>{
+    app.delete('/coffees/:id', async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const query = { _id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = coffeeCollection.deleteOne(query);
       res.send(result);
+    })
+
+    // Update a data
+    app.put('/coffees/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+      const result = coffeeCollection.updateOne(filter, options, coffee);
+      res.send(result)
     })
 
 
